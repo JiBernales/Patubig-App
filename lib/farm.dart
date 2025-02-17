d8c57cf1242d23b91ef5cd0fb24f41ba5721c02c
JiBernales <114903281+JiBernales@users.noreply.github.com>
2025-02-17 23:42:50 +0800

    Added map

--- lib/farm.dart
+++ lib/farm.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:intl/intl.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:weather/weather.dart';
 import 'main.dart';
 
@@ -134,16 +136,114 @@ class MapCard extends StatefulWidget {
 }
 
 class _MapCardState extends State<MapCard> {
+  late MapController _mapController;
+  late LatLng _currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    _mapController = MapController();
+    _currentPosition = LatLng(10.866519, 122.729620);
+    _loadLocation();
+  }
+
+  Future<void> _loadLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    setState(() {
+      _currentPosition = LatLng(position.latitude, position.longitude);
+    });
+  }
+
+  void _zoomIn() {
+    double currentZoom = _mapController.camera.zoom;
+  _mapController.move(LatLng(_currentPosition.latitude!, _currentPosition.longitude!), currentZoom + 0.5);
+  }
+
+  void _zoomOut() {
+    double currentZoom = _mapController.camera.zoom;
+  _mapController.move(LatLng(_currentPosition.latitude!, _currentPosition.longitude!), currentZoom - 0.5);
+  }
+
+  void _centerOnMarker() {
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(16),
       child: Container(
-        height: MediaQuery.of(context).size.height * 0.4,
+        height: MediaQuery
+            .of(context)
+            .size
+            .height * 0.4,
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(12),
-          boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 4, spreadRadius: 1)],
+          boxShadow: [
+            BoxShadow(color: Colors.black12, blurRadius: 4, spreadRadius: 1)
+          ],
+        ),
+        child: _currentPosition == null
+            ? Center(child: CircularProgressIndicator())
+            : ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: Stack(
+            children: [
+              FlutterMap(
+                mapController: _mapController,
+                options: MapOptions(
+                  initialCenter: _currentPosition,
+                  initialZoom: 13.0,
+                ),
+                children: [
+                  TileLayer(
+                    urlTemplate: 'https://tile-{s}.openstreetmap.fr/hot/{z}/{x}/{y}.png',
+                  ),
+                  MarkerLayer(
+                    markers: [
+                      Marker(
+                        point: LatLng(_currentPosition.latitude!, _currentPosition.longitude!),
+                        child: Icon(
+                          Icons.location_pin,
+                          color: Colors.red,
+                        ),
+                      ),
+                      Marker(
+                        point: LatLng(10.869182, 122.728615),
+                        child: Icon(Icons.sensors, color: Colors.red),
+                      ),
+                      Marker(
+                        point: LatLng(10.865102, 122.734650),
+                        child: Icon(Icons.sensors, color: Colors.red),
+                      ),
+                      Marker(
+                        point: LatLng(10.862329, 122.726496),
+                        child: Icon(Icons.sensors, color: Colors.red),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Positioned(
+                child: FloatingActionButton(
+                    onPressed: _zoomIn, child: Icon(Icons.add, color: Colors.black,),
+                  backgroundColor: Colors.white,
+                ),
+                bottom: 75,
+                right: 10,
+              ),
+              Positioned(
+                child: FloatingActionButton(
+                    onPressed: _zoomOut, child: Icon(Icons.remove, color: Colors.black,),
+                    backgroundColor: Colors.white,
+                ),
+                bottom: 10,
+                right: 10,
+              ),
+            ],
+          )
         ),
       ),
     );
